f641dd30339c280f3d545ab7609146fbb0daf6ef
Michael Marango <mike.marango@gmail.com>
2020-04-11 15:01:48 +0300

    Database context

--- src/ContosoUniversity-MVC-3.x/ContosoUniversity/Controllers/HomeController.cs
+++ src/ContosoUniversity-MVC-3.x/ContosoUniversity/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ContosoUniversity.Models;
+using ContosoUniversity.ViewModels;
 
 namespace ContosoUniversity.Controllers
 {
