56f43f0ea43fb5270d48d80f563337c3c309fbbc
Michael Marango <mike.marango@gmail.com>
2020-04-11 16:11:53 +0300

    Customize details page

--- src/ContosoUniversity-MVC-3.x/ContosoUniversity/Controllers/StudentsController.cs
+++ src/ContosoUniversity-MVC-3.x/ContosoUniversity/Controllers/StudentsController.cs
@@ -34,7 +34,10 @@ namespace ContosoUniversity.Controllers
             }
 
             var student = await _context.Students
+                .Include(s => s.Enrollments)
+                    .ThenInclude(e => e.Course)
                 .FirstOrDefaultAsync(m => m.ID == id);
+
             if (student == null)
             {
                 return NotFound();
